5f5f8f888c23f2c2582abfb5d9256c67f76fb553
luffy <prnshv69@gmail.com>
2018-05-22 13:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
